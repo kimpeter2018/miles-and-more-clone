0862b2be4f56de46eb1213d83c684b1b46cfa08b
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-01-07 15:34:14 +0100

    add riverpod dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  void main() {
+    runApp(
+      // For widgets to be able to read providers, we need to wrap the entire
+      // application in a "ProviderScope" widget.
+      // This is where the state of our providers will be stored.
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    );
+}
 }
 
 class MyApp extends StatelessWidget {
@@ -16,8 +26,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Firebase App',
       home: Scaffold(
-        appBar: AppBar(title: Text('Firebase Setup')),
-        body: Center(child: Text('Firebase Initialized!')),
+        appBar: AppBar(title: const Text('Firebase Setup')),
+        body: const Center(child: Text('Firebase Initialized!')),
       ),
     );
   }
